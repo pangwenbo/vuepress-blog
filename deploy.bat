918a257e99d4a6201365bd65130cd8c3bd1551fd
p_wenbo <102557135@qq.com>
2020-07-08 09:06:31 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -7,13 +7,9 @@ echo building-complete.
 git init
 git add -A
 git commit -m 'auto-deploy'
-git push -f git@github.com:pangwenbo/vuepress-blog.git master:gh-pages
+git remote add origin git@github.com:pangwenbo/vuepress-blog.git
+git pull
+git push --force origin HEAD:gh-pages
 
-REM To delete the dist folder
-
-echo delete-directory: "vuepress-blog"
-rmdir /s /q "vuepress-blog"
-cd..
-cd..
 echo Auto-Deploy-Complete!
 pause
