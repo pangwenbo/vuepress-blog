32efb6b82f70e8814cd5b4f32d7fb91cf73f3fff
p_wenbo <102557135@qq.com>
2020-07-08 08:52:51 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,6 @@ echo building...
 call build.bat
 echo building-complete.
 
-cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'auto-deploy'
@@ -13,7 +12,7 @@ git pull
 git push --force origin HEAD:gh-pages
 
 REM To delete the dist folder
-cd ..
+
 echo delete-directory: "%cd%/dist"
 rmdir /s /q "%cd%/dist"
 cd..
