24b5dc1416fd9d164b1ce517b0ce5cad2f0568ce
p_wenbo <102557135@qq.com>
2020-07-08 08:47:50 +0800

    'auto-deploy'

---
+++ docs/guide/sidebar.js
@@ -0,0 +1,16 @@
+module.exports = [
+		{
+		  title:'vue相关',
+		  collapsable: true,
+		  children:[
+			'/guide/notes/vue/websocket',
+		  ]
+		},
+		{
+		  title:'react相关',
+		  collapsable: true,
+		  children:[
+			'/guide/notes/react/ceshi',
+		  ]
+		}
+	]
